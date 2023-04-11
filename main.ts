5912e823b1f68451d73eb033e48aa106f639dbf8
Eino Siipi <101316567+EinoFin@users.noreply.github.com>
2023-04-11 17:29:23 +0300

    "it just works" -todd howard

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     serial.writeValue("temperature", weatherbit.temperature())
     serial.writeValue("humidity", weatherbit.humidity())
     serial.writeValue("pressure", weatherbit.pressure())
     serial.writeValue("altitude", weatherbit.altitude())
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     serial.writeValue("soil temperature", weatherbit.soilTemperature())
     serial.writeValue("soil moisture", weatherbit.soilMoisture())
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     serial.writeValue("wind speed", weatherbit.windSpeed())
     basic.pause(300)
     serial.writeValue("rain", weatherbit.rain())
@@ -16,8 +16,4 @@ input.onButtonPressed(Button.B, function () {
 weatherbit.startRainMonitoring()
 weatherbit.startWindMonitoring()
 weatherbit.startWeatherMonitoring()
-serial.redirect(
-SerialPin.P15,
-SerialPin.P14,
-BaudRate.BaudRate9600
-)
+serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600)
