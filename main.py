2bfef42ff9f5aec7adab8698cde4dfea5ccc69e0
Eino Siipi <101316567+EinoFin@users.noreply.github.com>
2023-04-12 11:59:38 +0300

    Päivitys main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-def on_button_pressed_a():
-    serial.write_value("temperature", weatherbit.temperature())
-    serial.write_value("humidity", weatherbit.humidity())
-    serial.write_value("pressure", weatherbit.pressure())
-    serial.write_value("altitude", weatherbit.altitude())
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_ab():
-    serial.write_value("soil temperature", weatherbit.soil_temperature())
+def soil():
+    serial.write_value("soil temperature", weatherbit.soil_temperature() / 100)
     serial.write_value("soil moisture", weatherbit.soil_moisture())
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def on_button_pressed_b():
+def wind_rain():
     serial.write_value("wind speed", weatherbit.wind_speed())
     basic.pause(300)
     serial.write_value("rain", weatherbit.rain())
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
+def bme280():
+    serial.write_value("temperature", weatherbit.temperature() / 100)
+    serial.write_value("humidity", weatherbit.humidity() / 1024)
+    serial.write_value("pressure", weatherbit.pressure() / 25600)
 weatherbit.start_rain_monitoring()
 weatherbit.start_wind_monitoring()
 weatherbit.start_weather_monitoring()
--- main.ts
+++ main.ts
@@ -1,18 +1,20 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    serial.writeValue("temperature", weatherbit.temperature())
-    serial.writeValue("humidity", weatherbit.humidity())
-    serial.writeValue("pressure", weatherbit.pressure())
-    serial.writeValue("altitude", weatherbit.altitude())
-})
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    serial.writeValue("soil temperature", weatherbit.soilTemperature())
+function soil() {
+    serial.writeValue("soil temperature", weatherbit.soilTemperature() / 100)
     serial.writeValue("soil moisture", weatherbit.soilMoisture())
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+}
+
+function wind_rain() {
     serial.writeValue("wind speed", weatherbit.windSpeed())
     basic.pause(300)
     serial.writeValue("rain", weatherbit.rain())
-})
+}
+
+function bme280() {
+    serial.writeValue("temperature", weatherbit.temperature() / 100)
+    serial.writeValue("humidity", weatherbit.humidity() / 1024)
+    serial.writeValue("pressure", weatherbit.pressure() / 25600)
+}
+
 weatherbit.startRainMonitoring()
 weatherbit.startWindMonitoring()
 weatherbit.startWeatherMonitoring()
