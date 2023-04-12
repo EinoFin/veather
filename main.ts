aea45cfd7aee7c8e87e007645dedb2fe04991ae3
Eino Siipi <101316567+EinoFin@users.noreply.github.com>
2023-04-12 14:17:55 +0300

    Päivitys pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,33 @@
-function soil() {
-    //  multa
+function soil () {
+    // multa
     serial.writeValue("soil temperature", weatherbit.soilTemperature() / 100)
     serial.writeValue("soil moisture", weatherbit.soilMoisture())
 }
-
-function wind_rain() {
-    //  tuuli ja sade
+function wind_rain () {
+    // tuuli ja sade
     serial.writeValue("wind speed", weatherbit.windSpeed())
     serial.writeValue("rain", weatherbit.rain())
 }
-
-function bme280() {
-    //  lämpö, kosteus ja ilmanpaine
+function bme280 () {
+    // lämpö, kosteus ja ilmanpaine
     serial.writeValue("temperature", weatherbit.temperature() / 100)
     serial.writeValue("humidity", weatherbit.humidity() / 1024)
     serial.writeValue("pressure", weatherbit.pressure() / 25600)
 }
-
-//  aloitus
+// aloitus
 weatherbit.startRainMonitoring()
 weatherbit.startWindMonitoring()
 weatherbit.startWeatherMonitoring()
-serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600)
+serial.redirect(
+SerialPin.P15,
+SerialPin.P14,
+BaudRate.BaudRate9600
+)
 timeanddate.set24HourTime(14, 3, 0)
 timeanddate.setDate(4, 12, 2023)
-loops.everyInterval(300000, function on_every_interval() {
-    //  tekee joka 5:s minuutti
-    serial.writeLine(timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr))
+loops.everyInterval(300000, function () {
+    // tekee joka 5:s minuutti
+    serial.writeLine(timeanddate.time(timeanddate.TimeFormat.HHMM24hr))
     soil()
     bme280()
     wind_rain()
