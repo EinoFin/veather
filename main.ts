4df330f56fdae3a4fba784bc803ab93805e6c20b
Eino Siipi <101316567+EinoFin@users.noreply.github.com>
2023-04-12 11:59:57 +0300

    Päivitys pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,22 @@
-function soil() {
+function soil () {
     serial.writeValue("soil temperature", weatherbit.soilTemperature() / 100)
     serial.writeValue("soil moisture", weatherbit.soilMoisture())
 }
-
-function wind_rain() {
+function wind_rain () {
     serial.writeValue("wind speed", weatherbit.windSpeed())
     basic.pause(300)
     serial.writeValue("rain", weatherbit.rain())
 }
-
-function bme280() {
+function bme280 () {
     serial.writeValue("temperature", weatherbit.temperature() / 100)
     serial.writeValue("humidity", weatherbit.humidity() / 1024)
     serial.writeValue("pressure", weatherbit.pressure() / 25600)
 }
-
 weatherbit.startRainMonitoring()
 weatherbit.startWindMonitoring()
 weatherbit.startWeatherMonitoring()
-serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600)
+serial.redirect(
+SerialPin.P15,
+SerialPin.P14,
+BaudRate.BaudRate9600
+)
