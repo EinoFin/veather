ef38b5f3a3490222b0a2529d7e33aa0c855e18e9
Eino Siipi <101316567+EinoFin@users.noreply.github.com>
2023-04-14 17:24:37 +0300

    Päivitys pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 def soil():
+    serial.write_value("Soil moisture", weatherbit.soil_moisture())
     # multa
-    serial.write_value("soil temperature", weatherbit.soil_temperature() / 100)
-    serial.write_value("soil moisture", weatherbit.soil_moisture())
+    serial.write_value("Soil temp", weatherbit.soil_temperature() / 100)
 def wind_rain():
     # tuuli ja sade
-    serial.write_value("wind speed", weatherbit.wind_speed())
-    serial.write_value("rain", weatherbit.rain())
+    serial.write_value("Wind speed", weatherbit.wind_speed())
+    serial.write_line("Wind direction:" + weatherbit.direction_string(0))
+    serial.write_value("Rain amount", weatherbit.rain())
 def bme280():
     # lämpö, kosteus ja ilmanpaine
-    serial.write_value("temperature", weatherbit.temperature() / 100)
-    serial.write_value("humidity", weatherbit.humidity() / 1024)
-    serial.write_value("pressure", weatherbit.pressure() / 25600)
+    serial.write_value("Temperature", weatherbit.temperature() / 100)
+    serial.write_value("Humidity", weatherbit.humidity() / 1024)
+    serial.write_value("Pressure", weatherbit.pressure() / 25600)
 # aloitus
 weatherbit.start_rain_monitoring()
 weatherbit.start_wind_monitoring()
 weatherbit.start_weather_monitoring()
 serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE9600)
-timeanddate.set24_hour_time(14, 3, 0)
-timeanddate.set_date(4, 12, 2023)
+timeanddate.set24_hour_time(15, 30, 0)
+timeanddate.set_date(4, 14, 2023)
 
 def on_every_interval():
     # tekee joka 5:s minuutti
-    serial.write_line(timeanddate.time(timeanddate.TimeFormat.HHMMSS2_4HR))
-    soil()
+    serial.write_line("")
+    serial.write_line("["+timeanddate.date_time()+"]")
     bme280()
+    soil()
     wind_rain()
-loops.every_interval(300000, on_every_interval)
+loops.every_interval(60000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,34 +1,35 @@
-function soil () {
-    // multa
-    serial.writeValue("soil temperature", weatherbit.soilTemperature() / 100)
-    serial.writeValue("soil moisture", weatherbit.soilMoisture())
+function soil() {
+    serial.writeValue("Soil moisture", weatherbit.soilMoisture())
+    //  multa
+    serial.writeValue("Soil temp", weatherbit.soilTemperature() / 100)
 }
-function wind_rain () {
-    // tuuli ja sade
-    serial.writeValue("wind speed", weatherbit.windSpeed())
-    serial.writeValue("rain", weatherbit.rain())
+
+function wind_rain() {
+    //  tuuli ja sade
+    serial.writeValue("Wind speed", weatherbit.windSpeed())
+    serial.writeLine("Wind direction:" + weatherbit.directionString(0))
+    serial.writeValue("Rain amount", weatherbit.rain())
 }
-function bme280 () {
-    // lämpö, kosteus ja ilmanpaine
-    serial.writeValue("temperature", weatherbit.temperature() / 100)
-    serial.writeValue("humidity", weatherbit.humidity() / 1024)
-    serial.writeValue("pressure", weatherbit.pressure() / 25600)
+
+function bme280() {
+    //  lämpö, kosteus ja ilmanpaine
+    serial.writeValue("Temperature", weatherbit.temperature() / 100)
+    serial.writeValue("Humidity", weatherbit.humidity() / 1024)
+    serial.writeValue("Pressure", weatherbit.pressure() / 25600)
 }
-// aloitus
+
+//  aloitus
 weatherbit.startRainMonitoring()
 weatherbit.startWindMonitoring()
 weatherbit.startWeatherMonitoring()
-serial.redirect(
-SerialPin.P15,
-SerialPin.P14,
-BaudRate.BaudRate9600
-)
-timeanddate.set24HourTime(14, 3, 0)
-timeanddate.setDate(4, 12, 2023)
-loops.everyInterval(300000, function () {
-    // tekee joka 5:s minuutti
-    serial.writeLine(timeanddate.time(timeanddate.TimeFormat.HHMM24hr))
-    soil()
+serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600)
+timeanddate.set24HourTime(15, 30, 0)
+timeanddate.setDate(4, 14, 2023)
+loops.everyInterval(60000, function on_every_interval() {
+    //  tekee joka 5:s minuutti
+    serial.writeLine("")
+    serial.writeLine("[" + timeanddate.dateTime() + "]")
     bme280()
+    soil()
     wind_rain()
 })
