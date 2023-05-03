56f36330dfe683b061d6a1ab1f2f641f02c8c14b
Eino Siipi <101316567+EinoFin@users.noreply.github.com>
2023-05-03 10:03:38 +0300

    Päivitys main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-def soil():
-    serial.write_value("Soil moisture", weatherbit.soil_moisture())
-    # multa
-    serial.write_value("Soil temp", dstemp.celsius(DigitalPin.P12))
-def wind_rain():
-    # tuuli ja sade
-    serial.write_value("Wind speed", weatherbit.wind_speed())
-    serial.write_line("Wind direction:" + weatherbit.direction_string(0))
-    serial.write_value("Rain amount", weatherbit.rain())
-def bme280():
-    # lämpö, kosteus ja ilmanpaine
-    serial.write_value("Temperature", weatherbit.temperature() / 100)
-    serial.write_value("Humidity", weatherbit.humidity() / 1024)
-    serial.write_value("Pressure", weatherbit.pressure() / 25600)
 # aloitus
 weatherbit.start_rain_monitoring()
 weatherbit.start_wind_monitoring()
 weatherbit.start_weather_monitoring()
 serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE9600)
-timeanddate.set24_hour_time(15, 30, 0)
-timeanddate.set_date(4, 14, 2023)
 
 def on_every_interval():
-    # tekee joka 5:s minuutti
-    serial.write_line("")
-    serial.write_line("[" + timeanddate.date_time() + "]")
-    bme280()
-    soil()
-    wind_rain()
-loops.every_interval(10000, on_every_interval)
+    serial.write_line("" + str(weatherbit.temperature() / 100) + "," + str(weatherbit.pressure() / 25600) + "," + str(weatherbit.humidity() / 1024) + "," + str(weatherbit.wind_speed()) + "," + (weatherbit.direction_string(0)) + "," + str(weatherbit.rain()) + "," + str(weatherbit.soil_moisture()) + "," + str(dstemp.celsius(DigitalPin.P12)))
+loops.every_interval(60000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,20 +1,3 @@
-function soil () {
-    serial.writeValue("Soil moisture", weatherbit.soilMoisture())
-    // multa
-    serial.writeValue("Soil temp", dstemp.celsius(DigitalPin.P12))
-}
-function wind_rain () {
-    // tuuli ja sade
-    serial.writeValue("Wind speed", weatherbit.windSpeed())
-    serial.writeLine("Wind direction:" + weatherbit.directionString(0))
-    serial.writeValue("Rain amount", weatherbit.rain())
-}
-function bme280 () {
-    // lämpö, kosteus ja ilmanpaine
-    serial.writeValue("Temperature", weatherbit.temperature() / 100)
-    serial.writeValue("Humidity", weatherbit.humidity() / 1024)
-    serial.writeValue("Pressure", weatherbit.pressure() / 25600)
-}
 // aloitus
 weatherbit.startRainMonitoring()
 weatherbit.startWindMonitoring()
@@ -24,13 +7,6 @@ SerialPin.P15,
 SerialPin.P14,
 BaudRate.BaudRate9600
 )
-timeanddate.set24HourTime(15, 30, 0)
-timeanddate.setDate(4, 14, 2023)
-loops.everyInterval(10000, function () {
-    // tekee joka 5:s minuutti
-    serial.writeLine("")
-    serial.writeLine("[" + timeanddate.dateTime() + "]")
-    bme280()
-    soil()
-    wind_rain()
+loops.everyInterval(60000, function () {
+    serial.writeLine("" + weatherbit.temperature() / 100 + "," + ("" + weatherbit.pressure() / 25600) + "," + ("" + weatherbit.humidity() / 1024) + "," + ("" + weatherbit.windSpeed()) + "," + weatherbit.windDirectionRaw() + "," + ("" + weatherbit.rain()) + "," + ("" + weatherbit.soilMoisture()) + "," + ("" + dstemp.celsius(DigitalPin.P12)))
 })
